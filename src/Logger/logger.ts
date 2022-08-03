13c157a7bb9e5f9512c7ba5a968252ba4d1017ab
Pablo Moron <moronpablo94@gmail.com>
2022-08-02 21:59:58 -0300

    Mejoro el middleware para logear

--- src/Logger/logger.ts
+++ src/Logger/logger.ts
@@ -1,33 +1,87 @@
 import * as express from "express"
+import * as expressWinston from "express-winston"
 import * as winston from "winston"
 import * as wm from "winston-mongodb"
 import { getConfig } from "../server/environment"
 
 const config = getConfig("develop")
-export const logger = winston.createLogger({
+
+const logger = winston.createLogger({
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json(), winston.format.prettyPrint()),
     transports: [
         new winston.transports.Console({
-            level: "info",
-            format: winston.format.combine(winston.format.timestamp(), winston.format.json())
+            level: "info",        
         }),
         new winston.transports.File({
             dirname: "e:/logs/",
-            filename: "debug.log",
+            filename: `debug${new Date().toISOString().split('T')[0]}.log`,
             level: "debug",
         }),
         new wm.MongoDB({
-            
             db: config.mongoDB,
-            level: "error",
-            collection: "err",
-            options:{
-               useUnifiedTopology: true
-            }
+            level: "info",
+            collection: "info",
+            options: {
+                useUnifiedTopology: true
+            },
+            metaKey: "metadata"
         })
     ]
 })
-export const logRequest = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    logger.error(req.headers)
-    logger.debug("hi")
-    next()
-}
+
+
+//WINSTON EXPRESS
+export const ewLogger = expressWinston.logger({
+    msg: undefined,
+    /*
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json(), winston.format.prettyPrint()),
+    transports: [
+        new winston.transports.File({
+            dirname: "e:/logs/",
+            filename: `debug${new Date().toISOString().split('T')[0]}.log`,
+            level: "info",
+        }),
+        new winston.transports.Console(),
+        new wm.MongoDB({
+            db: config.mongoDB,
+            level: "info",
+            collection: "err",
+            options: {
+                useUnifiedTopology: true
+            }
+        })
+    ],
+    */
+    dynamicMeta: (req: express.Request, res: express.Response, err: Error): any => {
+        let response = {
+            response: {
+                origin_ipv6: req.ip.indexOf(':') >= 0 ? req.ip.substring(0, req.ip.lastIndexOf(':')) : req.ip,
+                origin_ipv4: req.ip.indexOf(':') >= 0 ? req.ip.substring(req.ip.lastIndexOf(':') + 1) : req.ip,
+                headers: JSON.parse(JSON.stringify(res.getHeaders())), // Evita [Object: null prototype]
+                status: `${res.statusCode}: ${res.statusMessage}`,
+                body: res.body,
+                local_date: new Date().toLocaleString()
+            }
+        }
+        return response;
+    },
+    requestWhitelist: ['headers', 'query', 'params', 'route.path', 'body'],
+    responseWhitelist: ['body'],
+    responseField: "response",
+    requestField: "request",
+    metaField: "metadata",
+    baseMeta: {},
+    winstonInstance: logger
+})
+
+export const errorLogger = expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            dirname: "e:/logs/",
+            filename: `errors_${new Date().toISOString().split('T')[0]}.log`,
+            level: "error",
+        }),
+    ],
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json(), winston.format.prettyPrint()),
+})
