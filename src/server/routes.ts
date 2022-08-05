0353b143b043427bd7af22a50e43753ed0400384
Pablo Moron <moronpablo94@gmail.com>
2022-08-04 23:15:06 -0300

    actualizo las rutas

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -2,6 +2,7 @@
 import * as express from "express"
 import {initSearchRoute} from "../routes/searchRoute";
 import {initItemsRouter} from "../routes/itemRoute"
+import passport = require("passport");
 
 export const init = (app: express.Express):void  => {
     //Aqui se puede iniciar otros routers
@@ -12,12 +13,17 @@ export const init = (app: express.Express):void  => {
 }
 
 const hello = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    throw new Error("!!!!💥");
+    // throw new Error("💥");
     
+    console.log(req.user)
     res.json({
         message: "Hello! ^^"
     })
 }
+interface Ireq extends express.Request{
+    b: string,
+    mock: boolean
+}
 
 let helloRouter = express.Router()
-helloRouter.post("/api/hello/:pathParam", hello)
+helloRouter.post("/api/hello/:pathParam", passport.authenticate('token', {session: false}), hello)
