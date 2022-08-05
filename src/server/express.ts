c093d5863d3355a6346f56eb60efe1e45116e037
Pablo Moron <moronpablo94@gmail.com>
2022-08-04 23:13:32 -0300

    Completo pipelines de las requests

--- src/server/express.ts
+++ src/server/express.ts
@@ -1,22 +1,33 @@
 import * as express from "express"
 import * as routes from "./routes"
 import { IConfig } from "./environment"
-import { ewLogger } from "../logger/logger"
 import * as cors from "cors"
 import { errorHandler, handle404 } from "./error"
+import passport = require("passport")
+import * as token from "../middlewares/passport"
+import { errorLogger, requestLogger } from "../middlewares/logger"
 
 export function initialize(config: IConfig): express.Express {
-    const app = express.default()
+    const app = express()
     app.disable("x-powered-by")
-    app.use(cors.default())
+    app.use(cors())
+
     app.use(express.urlencoded({
-        extended: true
+        extended: true,
+        limit: "5mb"
       }));
-    app.use(express.json())
-    app.use(ewLogger)
+
+    app.use(express.json({
+      limit: "5mb"
+    }))
+
+    app.use(passport.initialize());
+    token.init()
+
+    app.use(requestLogger)
     routes.init(app)
     app.use(handle404)
-    // app.use(errorLogger)
-    app.use(errorHandler);
+    app.use(errorLogger)
+    app.use(errorHandler)
     return app
 }
