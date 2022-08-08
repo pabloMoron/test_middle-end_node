c1a5f941f7581375a341fdc3748aabbafbb31bd7
Pablo Moron <moronpablo94@gmail.com>
2022-08-08 20:35:49 -0300

    Avance items endpoint

--- src/items/services.ts
+++ src/items/services.ts
@@ -1,20 +1,95 @@
 import * as express from "express"
+import { Source } from "../middlewares/passport"
+import * as error from "../server/error"
 
-export interface IItemDescriptionRequest extends express.Request {
-    id: String
+interface IItemDescriptionRequest extends express.Request {
+    user: Source;
 }
 
-export interface IItemDescriptionResponse{
-
+export interface IItemDescriptionResponse {
+    author: {
+        name: String
+        lastname: String
+    },
+    item: {
+        id: String,
+        title: String,
+        price: {
+            currency: String,
+            amount: Number,
+            decimals: Number,
+        },
+        picture: String,
+        condition: String,
+        free_shipping: Boolean,
+        sold_quantity: Number
+        description: String
+    }
 }
 
-export async function findItemDescriptionById (req: IItemDescriptionRequest, res: express.Response) {
-    // console.log(req.id)
-    res.json({a:"OK"})
+export async function findItemDescription (req: IItemDescriptionRequest, res: express.Response) {
+    try{
+        const itemDesc = await findItemDescriptionById(req.user, req.params.id)
+        res.json(itemDesc)
+    }
+    catch(exception){
+        error.handle(res, exception)
+    }
 }
 
-function validateRequest(req: IItemDescriptionRequest): Promise<IItemDescriptionRequest> {
-    const regex = /[A-Za-z0-9]/;
-    return Promise.reject("")
-    return Promise.resolve(req)
+async function findItemDescriptionById(source: Source, id: string): Promise<IItemDescriptionResponse>{
+    return new Promise<IItemDescriptionResponse>((resolve, reject)=>{
+        validateDescriptionRequest(id)
+        .then(source => {
+            console.log(source)
+            resolve({
+                author: {
+                    name: "Pablo",
+                    lastname: "Moron"
+                },
+                item:{
+                    condition: "new",
+                    description: "DS1 Bonfire paint 30cmx20cm",
+                    free_shipping: false,
+                    id: id,
+                    picture: "http://localhost:9000/ds1_bonfire.jpg",
+                    price: {
+                        amount: 100,
+                        currency: "dolars",
+                        decimals: 10
+                    },
+                    sold_quantity: 1,
+                    title: "Dark Souls bonfire paint"
+                },
+
+            })
+        })
+        .catch(exception => reject(exception))
+    })
+}
+
+function validateDescriptionRequest(id: string): Promise<string> {
+    const result: error.ValidationErrorMessage = {
+        messages: []
+    }
+    const regex = new RegExp("[^A-Za-z0-9]")
+    
+    if(!id || id.length<=0) {
+        result.messages.push({
+            path: "id",
+            message: "id cannot be null"
+        })
+    }
+
+    if( regex.test(id)) {
+        result.messages.push({
+            path: "id",
+            message: "numbers and letters only"
+        })
+    }
+
+    if(result.messages.length>0){
+        return Promise.reject(result)
+    }
+    return Promise.resolve(id)
 }
--- src/routes/itemRoute.ts
+++ src/routes/itemRoute.ts
@@ -1,14 +1,13 @@
 "use strict"
 
 import * as express from "express"
-import { findItemDescriptionById } from "../items/services"
-import * as items from "../items"
+import { findItemDescription } from "../items/services"
 
 const itemsRouter: express.IRouter = express.Router()
 
 export function initItemsRouter(app: express.Express): void {
     itemsRouter
     .route("/api/items/:id")
-    .get(findItemDescriptionById)
+    .get(findItemDescription)
     app.use(itemsRouter)
 }
