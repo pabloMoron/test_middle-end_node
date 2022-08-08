45a0d16ba538ac1a979fe912ffde11f984a590a7
Pablo Moron <moronpablo94@gmail.com>
2022-08-08 20:33:06 -0300

    Agregar funciones para manejar errores

--- src/server/error.ts
+++ src/server/error.ts
@@ -16,3 +16,56 @@ export function errorHandler(err: any, req: express.Request, res: express.Respon
         error: err.message
     })
 }
+
+export function handle(res: express.Response, err: any): express.Response {
+    if (err instanceof ValidationErrorMessage) {
+      // ValidationErrorMessage
+      if (err.code) {
+        res.status(err.code);
+      } else {
+        res.status(400);
+      }
+      const send: any = {};
+      if (err.error) {
+        send.error = err.error;
+      }
+      if (err.messages) {
+        send.messages = err.messages;
+      }
+  
+      return res.send(send);
+    } else {
+      return res.send(sendUnknown(res, err));
+    }
+  }
+
+  function sendUnknown(res: express.Response, err: any): ValidationErrorMessage {
+    res.status(500);
+    return { error: err };
+  }
+
+export class ValidationErrorMessage {
+    code?: number;
+    error?: string;
+    messages?: ValidationErrorItem []
+}
+
+export class ValidationErrorItem{
+    path: string;
+    message: string
+}
+export function newArgumentError(argument: string, err: string): ValidationErrorMessage {
+    const result = new ValidationErrorMessage();
+    result.messages = [{
+      path: argument,
+      message: err
+    }];
+    return result;
+  }
+  
+  export function newError(code: number, err: string): ValidationErrorMessage {
+    const result = new ValidationErrorMessage();
+    result.code = code;
+    result.error = err;
+    return result;
+  }
