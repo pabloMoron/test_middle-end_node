90ebb828cfd2a0e726ff6dea9865c05058ea7aac
Pablo Moron <moronpablo94@gmail.com>
2022-08-08 20:33:22 -0300

    Mejorar passport

--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -1,27 +1,36 @@
 import * as passport from "passport"
 import { UniqueTokenStrategy } from "passport-unique-token"
 
-export function init() {
-    passport.use(
-        new UniqueTokenStrategy({
-            tokenHeader: "token"
-        },(token, done) => {
-            console.log(token)
-            if (token === "e962f81a-4d42-4eb3-86cd-a25e7237c8dc") {
-                console.log("Consultar API")
-                return done(null, { api: true }, "datos de api")
-            }
-            if (token === "55a4639f-55e8-4e14-a6cc-b79977b20a4e") {
-                console.log("Devolver datos mockeados")
-                return done(null, { mock: true }, "datos mockeados")
-            }
+export interface Source  {
+    data_source: string;
+}
 
-            const err = {
-                name: "NO_VALID_TOKEN",
-                message: "Token invalido",
-                status: 401
+export function init() {
+    passport.use(new UniqueTokenStrategy({
+        tokenHeader: "token"
+    }, (token, done) => {
+        let source: Source
+        console.log(token)
+        if (token === "e962f81a-4d42-4eb3-86cd-a25e7237c8dc") {
+            console.log("Consultar API")
+            source = {
+                data_source: "api"
             }
-            return done(err)
-        })
+            return done(null, source)
+        }
+        if (token === "55a4639f-55e8-4e14-a6cc-b79977b20a4e") {
+            console.log("Devolver datos mockeados")
+            source = {
+                data_source: "api"
+            }
+            return done(null, source)
+        }
+        const err = {
+            name: "NO_VALID_TOKEN",
+            message: "Token invalido",
+            status: 401
+        }
+        return done(err)
+    })
     )
 }
